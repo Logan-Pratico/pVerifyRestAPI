57925889f9820fc650180dabf5f473c6a26be7b1
Logan <Logan@YOGA-900S>
2017-10-26 09:20:42 -0400

    Updates to API For Viewing (not final)

--- pVerifyRestApiJava/src/GetToken.java
+++ pVerifyRestApiJava/src/GetToken.java
@@ -9,11 +9,13 @@ import okhttp3.Response;
 
 public class GetToken {
 	
-	OkHttpClient client = new OkHttpClient();
-	MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-	RequestBody body = RequestBody.create(mediaType, "username=pverify_demo&password=pverify@949&grant_type=password");
+	private OkHttpClient client = new OkHttpClient();
+	private MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+	private RequestBody body = RequestBody.create(mediaType, "username=pverify_demo&password=pverify@949&grant_type=password");
+	private TokenResponse tokenResponse = new TokenResponse();
+	private String token;
 	
-public String run(String url) throws IOException{
+public void setToken(String url) throws IOException{
 		Request request = new Request.Builder().url(url)
 	  .post(body)
 	  .addHeader("content-type", "application/x-www-form-urlencoded")
@@ -21,19 +23,66 @@ public String run(String url) throws IOException{
 	  .addHeader("postman-token", "a8b54837-13aa-29db-47ea-793130984de5")
 	  .build();
 		try(Response response = client.newCall(request).execute()){
-			return response.body().string();
+			token = response.body().string();
+			scanToken(token);
+
 		}
 	}
 	
+	public String getToken() {
+		return token;
+	}
+
+	public TokenResponse getTokenResponse() {
+		return tokenResponse;
+	}
+	
+public void scanToken(String input) {
+	int a = 0;
+	String newToken;
+	int product = 1;
+	for (int i = 0; i < input.length(); i++) {
+		if (input.charAt(i) != '"') {
+			int k = indexOf(input, '"', i);
+			if (k == -1) {
+
+				newToken = input.substring(i + 1, input.length()-1);
+				tokenResponse.setExpiresIn(newToken);
+
+				return;
+				
+				
+			}
+			else {
+
+			newToken = input.substring(i,k);
+			if(a == 3) {
+				
+				tokenResponse.setAccessToken(newToken);
+			}else if(a == 7) {
+				tokenResponse.setTokenType(newToken);
+			}
+			a++;
+			i = k;
+			}
+		}
+	}
+	return;
+}
+
+private static int indexOf(String s, char ch, int fromIndex) {
+	
+	for (int i = fromIndex; i < s.length(); i++) {
+		if (s.charAt(i) == ch) {
+			return i;
+		}
+
+	}
+	return -1;
+}
+	
 	
 
-	
-	
-public static void main(String[] args) throws IOException {
-		GetToken token = new GetToken();
-		String response = token.run("https://www.pverify.net/TestEligibilityRest/Token");
-		System.out.println(response);
-	}
 	
 	}
 
